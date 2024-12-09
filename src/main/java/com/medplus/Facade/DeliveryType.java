907eccc2794ba2a40af539c9c55b65e5cabd44fa
swamyms <mudiga.s@northeastern.edu>
2024-12-09 01:02:51 -0500

    enum delivery type

---
+++ src/main/java/com/medplus/Facade/DeliveryType.java
@@ -0,0 +1,22 @@
+package com.medplus.Facade;
+
+public enum DeliveryType {
+	Delivery,
+	Pickup;
+	
+    public static DeliveryType getDeliveryType(String s) {
+    	DeliveryType result = null;
+    	switch(s.toLowerCase()) {
+    	case "delivery":
+    		result = DeliveryType.Delivery;
+    		break;
+	    case "pickup":
+	    	result = DeliveryType.Pickup;
+	    	break;
+		default:
+			result = DeliveryType.Pickup;
+			break;
+		}
+    	return result;
+    }
+}
